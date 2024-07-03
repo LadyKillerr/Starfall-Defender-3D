016359f91017a8034b59f90615f940341c050ca1
Lady Killerr <dungvuduc0509@gmail.com>
2024-07-03 23:52:09 +0700

    tweak and tune the game

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,15 +3,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    float horizontalThrow;
+    float verticalThrow;
+
     [SerializeField] InputAction movement;
-    
-    [Header("Move Speed Tuning")]
+
+    [Header("Movement Tuning")]
     [SerializeField] float controlSpeed = 10f;
 
     [SerializeField] float clampedXRange = 10f;
     [SerializeField] float clampedYMin = 0f;
-    [SerializeField] float clampedYMax = 10f;
+    [SerializeField] float clampedYMax = 15f;
 
+    [Header("Rotation Tuning")]
+    [SerializeField] float pitchFactor = -2f;
+    [SerializeField] float pitchTurnSpeed = -10f;
+
+    [SerializeField] float YawFactor = 2f;
+    //[SerializeField] float yawTurnSpeed = 15f;
+
+    [SerializeField] float rollFactor = -15f;
 
     void Awake()
     {
@@ -28,7 +39,7 @@ public class PlayerController : MonoBehaviour
         movement.Disable();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         Fly();
 
@@ -37,9 +48,10 @@ public class PlayerController : MonoBehaviour
     void Fly()
     {
         // Nhận Input từ bàn phím 
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+         horizontalThrow = movement.ReadValue<Vector2>().x;
+         verticalThrow = movement.ReadValue<Vector2>().y;
 
+        #region ShipMovement
         // Tạo biến offset để gán vào position mới của Ship
         float xOffset = horizontalThrow * controlSpeed * Time.deltaTime;
         float yOffset = verticalThrow * controlSpeed * Time.deltaTime;
@@ -54,7 +66,26 @@ public class PlayerController : MonoBehaviour
 
         // gán vào vị trí mới 
         transform.localPosition = new Vector3(clampedXpos, clampedYPos, transform.localPosition.z);
-            
+        #endregion
+
+        #region ShipRotation
+
+        float pitchDueToPosition = transform.localPosition.y * pitchFactor;
+        float pitchDueToMovement = verticalThrow * pitchTurnSpeed;
+
+        float yawDueToPosition = transform.localPosition.x * YawFactor;
+        //float yawDueToMovement = horizontalThrow * yawTurnSpeed;
+
+        //float rollDueToPosition 
+        float rollDueToMovement = transform.localPosition.z * horizontalThrow * rollFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToMovement;
+        float yaw = yawDueToPosition;
+        float roll = rollDueToMovement;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+
+        #endregion
 
     }
 
